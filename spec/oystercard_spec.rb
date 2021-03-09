9d5f09a4830b94ae84d40efcd6abfe915dbad105
jackmcc08 <jackmcc08@gmail.com>
2021-03-09 14:57:57 +0000

    completed challenge 6 - added limit to top up and max balance on oyster card.
    Co-authored-by: crotchetycrow <jackleighton@live.co.uk>

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,8 +5,16 @@ describe Oystercard do
   it "checks balance" do
     expect(subject).to respond_to(:balance)
   end
+
   it "tops up balance" do
     expect(subject).to respond_to(:top_up)
   end
 
+  it "tops up £50 and provide confirmation" do
+    expect(subject.top_up(50)).to eq "Card succesfully topped up. Balance is now £50."
+  end
+
+  it "cannot top up past the MAXIMUM LIMIT and raises an error" do
+    expect{subject.top_up(100)}.to raise_error "Card max limit is £#{Oystercard::MAXIMUM_LIMIT}. Top up failed, no money added."
+  end
 end
